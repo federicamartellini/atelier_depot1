ae6edc1b5b2febc9f026fa87ff130bfb0f9968ad
federicamartellini <martellini.f.e@gmail.com>
2025-01-03 18:13:14 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express'); //import
+const bodyParser = require('body-parser'); //import
+const cors = require('cors'); //import
+const mongoose = require('./database/connection'); //Connexion MongoDB
+const userRoutes = require('./routes/userRoutes'); // // Fichier de routes.
+const messageRoutes = require('./routes/messageRoutes'); // // Fichier de routes pour les messages
+
+const app = express(); //initialisation du server
+
+// Middleware
+app.use(bodyParser.json()); // Lire les données JSON dans les requêtes.
+app.use(cors()); // Gérer les permissions entre serveurs.
+
+// Routes
+app.use('/api/users', userRoutes); // Endpoint pour les utilisateurs.
+app.use('/api/messages', messageRoutes); // Endpoint pour les messages
+
+// Port 
+const PORT = process.env.PORT || 3000; //demarrage du server
+// Lancement du serveur
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+// Route de base
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur votre serveur!');
+});
